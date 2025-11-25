869a4b32d15cf1fcb16b2c35ce62e1521ea978c2
muhammedkulay-hub <muhammedkulay@gmail.com>
2025-11-25 03:46:39 +0300

    Add apps/mobile/src/navigation/AuthNavigator.js

---
+++ apps/mobile/src/navigation/AuthNavigator.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from '../screens/auth/LoginScreen';
+import RegisterScreen from '../screens/auth/RegisterScreen';
+import ForgotPasswordScreen from '../screens/auth/ForgotPasswordScreen';
+
+const Stack = createStackNavigator();
+
+const AuthNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Login"
+      screenOptions={{
+        headerShown: false,
+        cardStyle: { backgroundColor: '#0A0E27' },
+      }}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default AuthNavigator;
+
