d60b84e26ca20bbd500e65bbd627ab0d9b75240f
muhammedkulay-hub <muhammedkulay@gmail.com>
2025-11-25 03:49:12 +0300

    Add automation/n8n/__init__.py

---
+++ automation/n8n/__init__.py
@@ -0,0 +1,16 @@
+"""
+n8n Integration Module for AsilParla v1.17.02
+"""
+from .integrations import N8NIntegrations, get_integrations
+
+__all__ = ['N8NIntegrations', 'get_integrations']
+
+
+
+
+
+
+
+
+
+
